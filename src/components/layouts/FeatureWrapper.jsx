25c5b9b479c59a9b947974d8b2a06ece9a326419
Valerii Shkliar <walerik2848@gmail.com>
2026-01-23 15:37:45 +0200

    Fix featureWrapper bug i styles

--- src/components/layouts/FeatureWrapper.jsx
+++ src/components/layouts/FeatureWrapper.jsx
@@ -1,4 +1,5 @@
-const WRAPPER_CLASS = 'wrapper';
+import '../../global-styles/_reset.scss';
+const WRAPPER_CLASS = 'featureWrapper';
 
 function FeatureWrapper(props) {
   return <div className={WRAPPER_CLASS}>{props.children}</div>;
